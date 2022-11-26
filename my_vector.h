26b566609a5a7685c18b51c94b5b892443cfaf13
Дмитрий Воронецкий <vda1999@yandex.ru>
2022-11-26 15:54:15 +0400

    refactoring

--- my_vector.h
+++ my_vector.h
@@ -1,14 +1,56 @@
-//
-// Created by dima on 26.11.22.
-//
+#include <cstdlib>
+#include <sys/mman.h>
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <stdexcept>
 
-#ifndef CPPPROJECT_MY_VECTOR_H
-#define CPPPROJECT_MY_VECTOR_H
+using std::string;
+using std::runtime_error;
 
+template<typename T>
+class MyVector {
+public:
+    explicit MyVector(size_t max_capacity = 32768);
 
-class my_vector {
+    ~MyVector();
 
+    MyVector(const MyVector&) = delete;
+
+    MyVector(MyVector&& other) noexcept;
+
+    MyVector& operator = (const MyVector&) = delete;
+
+    MyVector& operator = (MyVector&& other)  noexcept;
+
+    T& operator [] (size_t ind);
+
+    const T& operator [] (size_t ind) const;
+
+    void pop();
+
+    void push_back(const T& element);
+
+    template<typename ...Args>
+    void emplace_back(Args ...args);
+
+private:
+    static const int PROT = PROT_WRITE | PROT_READ;
+    static int DEFAULT_FLAGS;
+
+    static size_t PAGE_SIZE;
+
+    void clear();
+
+    bool need_increasing();
+
+    void increase_capacity();
+
+    void fallback_allocate();
+
+    char* raw_data;
+    size_t pages_allocated;
+    size_t size;
 };
 
-
-#endif //CPPPROJECT_MY_VECTOR_H
+#include "my_vector.tpp"
